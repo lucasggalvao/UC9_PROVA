39556df2307fbf3d0309dd2d7faba20e9b3e66f9
lucasggalvao <lucas11galvao@gmail.com>
2023-03-03 20:09:59 -0300

    update

--- UC9_PROVA/Program.cs
+++ UC9_PROVA/Program.cs
@@ -1,10 +1,16 @@
 ﻿int i = 0;
-string aluno;
-double aulas = 0;
-double faltas = 0;
-double prova1;
-double prova2;
-double prova3;
+int FT = 0;
+int aulas = 0;
+int faltas = 0;
+string aluno = " ";
+double presenca = 0;
+double peso1 = 30;
+double peso2 = 25;
+double peso3 = 35;
+double prova1 = 0;
+double prova2 = 0;
+double prova3 = 0;
+double media;
 double trab;
 
 
@@ -12,12 +18,13 @@ double trab;
 
 for (i = 1; i < 5; i++) {
     Console.Clear();
+    
     Console.WriteLine("Informe o nome do aluno: ");
     aluno = Console.ReadLine();
     Console.WriteLine("Numero de aulas dadas: ");
-    aulas = double.Parse(Console.ReadLine());
+    aulas = int.Parse(Console.ReadLine());
     Console.WriteLine("Numero de faltas: ");
-    faltas = double.Parse(Console.ReadLine());
+    faltas = int.Parse(Console.ReadLine());
     Console.WriteLine("Informe a nota da " + i + "ª Prova: ");
     prova1 = double.Parse(Console.ReadLine());
     Console.WriteLine("Informe a nota da " + (i+1) + "ª Prova: ");
@@ -28,7 +35,31 @@ for (i = 1; i < 5; i++) {
     trab = double.Parse(Console.ReadLine());
 }
 
-    
+media =(prova1 * peso1 + prova2 * peso2 + prova3 * peso3) / (peso1 + peso2 + peso3);
+FT = (faltas * 100) / aulas;
+presenca = 100 - FT;
+
+if(media >= 6 && presenca > 25)
+{
+    Console.WriteLine(aluno + ", Aprovado com " + media.ToString("F") + " de media e " + presenca + " de menção");
+}
+if (media < 6 && presenca >= 25)
+{
+    Console.WriteLine(aluno + ", Reprovado por menção com " + media.ToString("F") + " de media");
+}
+if (media > 6 && presenca < 25)
+{
+    Console.WriteLine(aluno + ", Reprovado por faltas com " + presenca + " de menção");
+}
+if (media < 6 && presenca < 25)
+{
+    Console.WriteLine(aluno + ", Reprovado por menção e faltas com " + media.ToString("F") + " de media e " + presenca + " de menção");
+}
+
+Console.WriteLine("Pressione qualquer para continuar");
+Console.ReadLine();
+
+
 
 
 
