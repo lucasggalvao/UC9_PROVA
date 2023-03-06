e6484f632eb7848c7d375a601582fbc029c8d689
lucasggalvao <126626285+lucasggalvao@users.noreply.github.com>
2023-03-06 19:57:43 -0300

    Update Program.cs

--- UC9_PROVA/Program.cs
+++ UC9_PROVA/Program.cs
@@ -1,64 +1,72 @@
-﻿int i = 0;
-int FT = 0;
-int aulas = 0;
+string aluno;
+int i;
 int faltas = 0;
-string aluno = " ";
-double presenca = 0;
-double peso1 = 30;
-double peso2 = 25;
-double peso3 = 35;
+int faltastotal = 0;
+int aulas = 0;
 double prova1 = 0;
 double prova2 = 0;
 double prova3 = 0;
-double media;
-double trab;
+double trabalho = 0;
+double peso1 = 0.3;
+double peso2 = 0.25;
+double peso3 = 0.35;
+double media = 0;
+double presenca = 0;
+string resultado = "";
 
 
-
-
-for (i = 1; i < 5; i++) {
+for (i = 1; i <= 5; i++)
+{
     Console.Clear();
-    
-    Console.WriteLine("Informe o nome do aluno: ");
+
+    Console.WriteLine("Informe o nome do " + i + "º aluno");
     aluno = Console.ReadLine();
-    Console.WriteLine("Numero de aulas dadas: ");
+
+    Console.WriteLine("Informe a quantidade de aulas");
     aulas = int.Parse(Console.ReadLine());
-    Console.WriteLine("Numero de faltas: ");
+
+    Console.WriteLine("Informe a quantidade de faltas");
     faltas = int.Parse(Console.ReadLine());
-    Console.WriteLine("Informe a nota da " + i + "ª Prova: ");
+
+    Console.WriteLine("Informe a nota da prova 1");
     prova1 = double.Parse(Console.ReadLine());
-    Console.WriteLine("Informe a nota da " + (i+1) + "ª Prova: ");
+
+    Console.WriteLine("Informe a nota da prova 2");
     prova2 = double.Parse(Console.ReadLine());
-    Console.WriteLine("Informe a nota da " + (i + 1) + "ª Prova: ");
+
+    Console.WriteLine("Informe a nota da prova 3");
     prova3 = double.Parse(Console.ReadLine());
-    Console.WriteLine("Informe a nota do trabalho: ");
-    trab = double.Parse(Console.ReadLine());
-}
 
-media =(prova1 * peso1 + prova2 * peso2 + prova3 * peso3) / (peso1 + peso2 + peso3);
-FT = (faltas * 100) / aulas;
-presenca = 100 - FT;
+    Console.WriteLine("Informe a nota do trabalho");
+    trabalho = double.Parse(Console.ReadLine());
 
-if(media >= 6 && presenca > 25)
-{
-    Console.WriteLine(aluno + ", Aprovado com " + media.ToString("F") + " de media e " + presenca + " de menção");
-}
-if (media < 6 && presenca >= 25)
-{
-    Console.WriteLine(aluno + ", Reprovado por menção com " + media.ToString("F") + " de media");
-}
-if (media > 6 && presenca < 25)
-{
-    Console.WriteLine(aluno + ", Reprovado por faltas com " + presenca + " de menção");
-}
-if (media < 6 && presenca < 25)
-{
-    Console.WriteLine(aluno + ", Reprovado por menção e faltas com " + media.ToString("F") + " de media e " + presenca + " de menção");
-}
+    media = (prova1 * peso1 + prova2 * peso2 + prova3 * peso3) / (peso1 + peso2 + peso3) + trabalho;
+    faltastotal = (faltas * 100) / aulas;
+    presenca = 100 - faltastotal;
 
-Console.WriteLine("Pressione qualquer para continuar");
-Console.ReadLine();
 
+
+    if (media >= 6 && presenca >= 75)
+    {
+        Console.WriteLine(aluno + ", aprovado com " + md.ToString("F") + " de media e " + presenca + "% presença");
+    }
+    if (media < 6 && presenca >= 75)
+    {
+        Console.WriteLine(aluno + ", reprovado por notas com " + md.ToString("F") + " de media");
+    }
+    if (media > 6 && presenca < 75)
+    {
+        Console.WriteLine(aluno + ", reprovado por faltas com " + presenca + "% de presença");
+    }
+    if (media < 6 && presenca < 75)
+    {
+        Console.WriteLine(aluno + ", reprovado por menção e faltas com " + md.ToString("F") + " de media e " + presenca + "% de presença");
+    }
+
+    Console.WriteLine("Pressione qualquer trcla para continuar");
+    Console.ReadLine();
+
+}
 
 
 
